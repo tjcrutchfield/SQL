485f6ff23fc54be23b12be08370274e7271b8b9c
tjcrutchfield <45834868+tjcrutchfield@users.noreply.github.com>
2021-03-18 10:13:33 -0500

    Create indexUsage.sql

---
+++ indexUsage.sql
@@ -0,0 +1,12 @@
+--Unused indexes (Index usage)
+SELECT OBJECT_NAME(ix.object_ID) AS TableName, 
+       ix.name AS IndexName, 
+       ixs.user_seeks, 
+       ixs.user_scans, 
+       ixs.user_lookups, 
+       ixs.user_updates
+FROM sys.dm_db_index_usage_stats ixs
+     INNER JOIN sys.indexes ix ON ix.object_id = ixs.object_id
+                                  AND ixs.index_id = ixs.index_id
+WHERE OBJECTPROPERTY(ixs.object_id, 'IsUserTable')  = 1
+      AND ixs.database_id = DB_ID();
